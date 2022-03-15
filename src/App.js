3836081de8dd24b8a1308bcfd58b801b5c7390f3
Vitor Siqueira <vitormelosiqueira@gmail.com>
2022-03-15 20:52:14 -0300

    js code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {style} from './styles';
+
+const App = () => {
+  return (
+      <View style={style.container}>
+        
+      </View>
+  );
+};
+
+export default App;
