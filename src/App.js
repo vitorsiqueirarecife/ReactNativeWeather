3cce77b966482767c252bdeeceeaf4dc855ed9c2
Vitor Siqueira <vitormelosiqueira@gmail.com>
2022-03-15 21:29:15 -0300

    base styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {View} from 'react-native';
+import {TouchableOpacity, SafeAreaView, View, Text} from 'react-native';
 import {style} from './styles';
 
 const App = () => {
   return (
-      <View style={style.container}>
-        
-      </View>
+      <SafeAreaView style={style.wrapper}>
+          <View style={style.card}>
+            <Text style={style.title}>Localização atual</Text>
+          </View>
+          <View style={style.cardContent}>
+            <Text style={style.content}>........</Text>
+          </View>
+          <View style={style.card}>
+            <Text style={style.title}>Clima</Text>
+          </View>
+          <View style={style.cardContent}>
+            <Text style={style.content}>........</Text>
+          </View>            
+          <TouchableOpacity style={style.button} >
+            <Text style={style.title}>Atualizar</Text>
+          </TouchableOpacity>                 
+      </SafeAreaView>
   );
 };
 
