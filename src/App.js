1a30133d42cab24fd4b1e410c7793bb8bde4934c
Vitor Siqueira <vitormelosiqueira@gmail.com>
2022-03-16 23:01:14 -0300

    app mvp

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React, { useEffect } from 'react';
-import {TouchableOpacity, SafeAreaView, View, Text} from 'react-native';
-import {style} from './styles';
+import { TouchableOpacity, SafeAreaView, View, Text } from 'react-native';
+import { style } from './styles';
 import useLocation from './Hooks/useLocation';
+import useWeather from './Hooks/useWeather'
 
 const App = () => {
 
-  const { coordenates, error, loadPosition } = useLocation();	
-
+  const { loadPosition, coordenates} = useLocation();	
+  const { getWeather, informations } = useWeather();
+  
   useEffect(()=>{
-    loadPosition()
+    const run = async () => {
+      await loadPosition()
+    }
+    run()
   },[])
 
   useEffect(()=>{
-    console.log(coordenates)
+    const run = async () => {
+       await getWeather(coordenates?.latitude, coordenates?.longitude)     
+    }
+    run()
   },[coordenates])
 
   return (
       <SafeAreaView style={style.wrapper}>
           <View style={style.card}>
-            <Text style={style.title}>Localização atual</Text>
+            <Text style={style.title}>Location</Text>
           </View>
           <View style={style.cardContent}>
-            <Text style={style.content}>........</Text>
+            <Text style={style.content}>{informations?.name} - {informations?.sys?.country}</Text>
           </View>
           <View style={style.card}>
-            <Text style={style.title}>Clima</Text>
+            <Text style={style.title}>Weather</Text>
           </View>
           <View style={style.cardContent}>
-            <Text style={style.content}>........</Text>
+            <Text style={style.content}>{informations?.main?.temp} graus celsius ({informations?.weather?.[0]?.description})</Text>
+            <Text style={style.content}>Humidity: {informations?.main.humidity}</Text>
+            <Text style={style.content}>Pressure: {informations?.main.pressure}</Text>  
           </View>            
           <TouchableOpacity style={style.button} >
-            <Text style={style.title}>Atualizar</Text>
+            <Text style={style.title} onPress={()=> {
+              getWeather(coordenates?.latitude, coordenates?.longitude)   
+            }} >Atualizar</Text>
           </TouchableOpacity>                 
       </SafeAreaView>
   );
