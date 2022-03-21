d324696177a308335b0ccf3ce5b6c37f1304146f
Vitor Siqueira <vitormelosiqueira@gmail.com>
2022-03-21 18:53:21 -0300

    adjust test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,7 +2,9 @@ import 'react-native';
 import  React from 'react';
 import App from '../src/App';
 import renderer from 'react-test-renderer';
-// import {fireEvent, render} from '@testing-library/react-native';
+// import {render} from '@testing-library/react-native';
+
+//@TODO Improve tests and fix bugs.
 
 jest.mock('react-native-permissions', () => ({
   check: (_: boolean) => Promise.resolve(true),
@@ -18,15 +20,4 @@ it('renders correctly', () => {
 
 //   getByText('Seja bem-vindo!');
 //   getByText('Por favor, aguarde');
-// });
-
-// it('should add one more on array when press count button', () => {
-//   const {getByTestId, getByText} = render(<App />);
-
-//   fireEvent.press(getByTestId('countButton'));
-//   fireEvent.press(getByTestId('countButton'));
-
-//   getByText('(0)');
-//   getByText('(1)');
-//   getByText('(2)');
 // });
